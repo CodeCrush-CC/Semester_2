b25a7de12661b9ef7d14dffde438d3abf5344913
Tushar Malhotra <tusharmalhotra494@gmail.com>
2020-02-02 01:27:46 -0800

    First five experiments

---
+++ EXP_1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	float a, b, c ;
+	clrscr() ;
+
+	printf("\nName: Your Name \nRoll Number: your roll no.") ;
+
+	printf("\n\nEnter first number:\t") ;
+	scanf("%f", &a) ;
+
+	printf("\nEnter second number:\t") ;
+	scanf("%f", &b) ;
+
+	c = a+b ;
+
+	printf("\nSum:\t%f", c) ;
+
+	getch() ;
+}
